86d2d3cce3d01df5af70c594eb796d3ae5e5592d
Paulo Oliveira <paulo@seqoy.com>
2011-03-14 22:44:36 -0300

    DOCS Source Update

---
+++ Sources/JUMPHeaders.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2011 - SEQOY.org and Paulo Oliveira ( http://www.seqoy.org )
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * \mainpage JUMP Framework
+ <b>JUMP Framework</b> is a collection of classes in Objective-C to perform a series of tasks 
+ on iOS or Mac OS applications. From Network libraries until User Interface libraries. 
+ The framework have classes to accomplish day by day taks until very complex applications.
+ <p>
+ <h1>JUMP Framework Modules</h1>
+ <b>JUMP Framework</b> is divided on different <i>weakly coupled</i> modules. You can import all 
+ modules or only the modules that you want to use on your project.
+ <p>
+ This is are the main Modules:
+	- <a href="../../JUMPDatabase/html/index.html">Database Module</a>
+	- <a href="../../JUMPNetwork/html/index.html">Network Module</a>
+	- <a href="../../JUMPData/html/index.html">Data Module</a>
+	- <a href="../../JUMPUserInterface/html/index.html">User Interface Module</a>
+  
+ <br>
+ <br>
+ */
