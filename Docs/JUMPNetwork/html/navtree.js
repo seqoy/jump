98d8087b0e5fd38bfa2ac35854ee0498249f0e7b
Paulo Oliveira <Paulo@MacPeter.local>
2011-03-14 21:22:51 -0300

    Commit Docs

---
+++ Docs/JUMPNetwork/html/navtree.js
@@ -0,0 +1,404 @@
+var NAVTREE =
+[
+  [ "JUMPNetwork", "index.html", [
+    [ "JUMP Network Programming Guide", "index.html", [
+      [ "The Pipeline", "a00001.html", null ],
+      [ "Handlers", "a00003.html", null ],
+      [ "Event", "a00005.html", null ],
+      [ "Event Message", "a00006.html", [
+        [ "HTTP Event Messages", "a00007.html", null ],
+        [ "JSON-RPC Event Messages", "a00008.html", null ]
+      ] ],
+      [ "I|O Transporter", "a00002.html", null ],
+      [ "Using Factories", "a00004.html", null ]
+    ] ],
+    [ "Modules", "modules.html", [
+      [ "Pipeline Handlers", "a00084.html", null ],
+      [ "Pipeline Events", "a00085.html", null ],
+      [ "HTTP Transporter Components", "a00086.html", null ],
+      [ "JSON-RPC Transporter Components", "a00087.html", null ]
+    ] ],
+    [ "Class List", "annotated.html", [
+      [ "<JPDataProcessserJSON>", "a00009.html", null ],
+      [ "JPDefaultCancelEvent", "a00010.html", null ],
+      [ "JPDefaultHandlerContext", "a00011.html", null ],
+      [ "JPDefaultHTTPMessage", "a00012.html", null ],
+      [ "JPDefaultPipelineExceptionEvent", "a00013.html", null ],
+      [ "JPHTTPTransporter", "a00014.html", null ],
+      [ "JPJSONRPCDecoderHandler", "a00015.html", null ],
+      [ "JPJSONRPCEncoderHandler", "a00016.html", null ],
+      [ "JPJSONRPCEventFactory", "a00017.html", null ],
+      [ "JPJSONRPCMessage", "a00018.html", null ],
+      [ "JPPipeline", "a00019.html", null ],
+      [ "JPPipelineDefaultFuture", "a00020.html", null ],
+      [ "<JPPipelineDownstreamHandler>", "a00021.html", null ],
+      [ "JPPipelineDowstreamMessageEvent", "a00022.html", null ],
+      [ "<JPPipelineEvent>", "a00023.html", null ],
+      [ "<JPPipelineEventFactory>", "a00024.html", null ],
+      [ "<JPPipelineEventListener>", "a00025.html", null ],
+      [ "JPPipelineException", "a00026.html", null ],
+      [ "<JPPipelineExceptionEvent>", "a00027.html", null ],
+      [ "<JPPipelineFutureListener>", "a00028.html", null ],
+      [ "<JPPipelineHandler>", "a00029.html", null ],
+      [ "<JPPipelineHandlerContext>", "a00030.html", null ],
+      [ "<JPPipelineMessageEvent>", "a00031.html", null ],
+      [ "JPPipelineNotification", "a00032.html", null ],
+      [ "JPPipelineSimpleMessageEvent", "a00033.html", null ],
+      [ "<JPPipelineSink>", "a00034.html", null ],
+      [ "<JPPipelineUpstreamHandler>", "a00035.html", null ],
+      [ "JPPipelineUpstreamMessageEvent", "a00036.html", null ],
+      [ "JPSimplePipelineDownstreamHandler", "a00037.html", null ],
+      [ "JPSimplePipelineHandler", "a00038.html", null ],
+      [ "JPSimplePipelineUpstreamHandler", "a00039.html", null ],
+      [ "<JPTransporterHTTPMessage>", "a00040.html", null ],
+      [ "<JPTransporterJSONRPCMessage>", "a00041.html", null ],
+      [ "<JPTransporterMessage>", "a00042.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "<JPDataProcessserJSON>", "a00009.html", null ],
+      [ "JPPipeline", "a00019.html", null ],
+      [ "JPPipelineDefaultFuture", "a00020.html", null ],
+      [ "<JPPipelineEvent>", "a00023.html", [
+        [ "<JPPipelineExceptionEvent>", "a00027.html", [
+          [ "JPDefaultPipelineExceptionEvent", "a00013.html", null ]
+        ] ],
+        [ "<JPPipelineMessageEvent>", "a00031.html", [
+          [ "JPPipelineSimpleMessageEvent", "a00033.html", [
+            [ "JPPipelineDowstreamMessageEvent", "a00022.html", [
+              [ "JPDefaultCancelEvent", "a00010.html", null ]
+            ] ],
+            [ "JPPipelineUpstreamMessageEvent", "a00036.html", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "<JPPipelineEventFactory>", "a00024.html", [
+        [ "JPJSONRPCEventFactory", "a00017.html", null ]
+      ] ],
+      [ "<JPPipelineEventListener>", "a00025.html", null ],
+      [ "JPPipelineException", "a00026.html", null ],
+      [ "<JPPipelineFutureListener>", "a00028.html", null ],
+      [ "<JPPipelineHandler>", "a00029.html", [
+        [ "<JPPipelineDownstreamHandler>", "a00021.html", [
+          [ "JPSimplePipelineDownstreamHandler", "a00037.html", [
+            [ "JPJSONRPCEncoderHandler", "a00016.html", null ]
+          ] ],
+          [ "JPSimplePipelineHandler", "a00038.html", null ]
+        ] ],
+        [ "<JPPipelineUpstreamHandler>", "a00035.html", [
+          [ "JPSimplePipelineHandler", "a00038.html", null ],
+          [ "JPSimplePipelineUpstreamHandler", "a00039.html", [
+            [ "JPJSONRPCDecoderHandler", "a00015.html", null ]
+          ] ]
+        ] ]
+      ] ],
+      [ "<JPPipelineHandlerContext>", "a00030.html", [
+        [ "JPDefaultHandlerContext", "a00011.html", null ]
+      ] ],
+      [ "JPPipelineNotification", "a00032.html", null ],
+      [ "<JPPipelineSink>", "a00034.html", [
+        [ "JPHTTPTransporter", "a00014.html", null ]
+      ] ],
+      [ "<JPTransporterMessage>", "a00042.html", [
+        [ "<JPTransporterHTTPMessage>", "a00040.html", [
+          [ "JPDefaultHTTPMessage", "a00012.html", [
+            [ "JPJSONRPCMessage", "a00018.html", null ]
+          ] ],
+          [ "<JPTransporterJSONRPCMessage>", "a00041.html", [
+            [ "JPJSONRPCMessage", "a00018.html", null ]
+          ] ]
+        ] ]
+      ] ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPDataProcessserJSON.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPDefaultCancelEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPDefaultHandlerContext.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPDefaultHTTPMessage.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPDefaultPipelineExceptionEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPHTTPTransporter.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPJSONRPCDecoderHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPJSONRPCEncoderHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPJSONRPCEventFactory.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPJSONRPCMessage.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipeline.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineCompleteFuture.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineDefaultFuture.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineDownstreamHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineDowstreamMessageEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineEventFactory.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineEventListener.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineException.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineExceptionEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineFactory.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineFailedFuture.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineFuture.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineFutureListener.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineHandlerContext.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineListener.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineListenerNotification.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineMessageEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineNotification.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineSimpleMessageEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineSink.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineSucceededFuture.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineUpstreamHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPPipelineUpstreamMessageEvent.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPSimplePipelineDownstreamHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPSimplePipelineHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPSimplePipelineUpstreamHandler.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPTransporterHTTPMessage.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPTransporterJSONRPCMessage.h", null, null ],
+      [ "/Users/Paulo/Projects/JUMP/JUMPNetwork/Headers/JPTransporterMessage.h", null, null ]
+    ] ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
