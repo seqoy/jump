dac9600d067835cc8c078bee4987e3ba7f3d4e85
Paulo Oliveira <paulo@seqoy.com>
2011-03-25 18:16:03 -0300

    Link Headers Conditional Folders

--- JUMPLinkHeaders.sh
+++ JUMPLinkHeaders.sh
@@ -12,22 +12,46 @@ fi
 echo "Acessing Main Headers Directory..."
 cd Headers
 
-echo "Linking JUMPCore Headers..."
-ln -s ../JUMPCore/Headers/* .
+######## ######## ######## ######## ######## ######## ########  
+# if JUMPCore Directory exist...
+if [ -d "../JUMPCore/Headers" ]; then
+	echo "Linking JUMPCore Headers..."
+	ln -s ../JUMPCore/Headers/* .
+fi
 
-echo "Linking JUMPData Headers..."
-ln -s ../JUMPData/Headers/* .
+######## ######## ######## ######## ######## ######## ########  
+# if JUMPCore Directory exist...
+if [ -d "../JUMPData/Headers" ]; then
+	echo "Linking JUMPData Headers..."
+	ln -s ../JUMPData/Headers/* .
+fi
 
-echo "Linking JUMPDatabase Headers..."
-ln -s ../JUMPDatabase/Headers/* .
+######## ######## ######## ######## ######## ######## ########  
+# if JUMPCore Directory exist...
+if [ -d "../JUMPDatabase/Headers" ]; then
+	echo "Linking JUMPDatabase Headers..."
+	ln -s ../JUMPDatabase/Headers/* .
+fi
 
-echo "Linking JUMPNetwork Headers..."
-ln -s ../JUMPNetwork/Headers/* .
+######## ######## ######## ######## ######## ######## ########  
+# if JUMPCore Directory exist...
+if [ -d "../JUMPNetwork/Headers" ]; then
+	echo "Linking JUMPNetwork Headers..."
+	ln -s ../JUMPNetwork/Headers/* .
+fi
 
-echo "Linking JUMPUserInterface Headers..."
-ln -s ../JUMPUserInterface/Headers/* .
+######## ######## ######## ######## ######## ######## ########  
+# if JUMPCore Directory exist...
+if [ -d "../JUMPUserInterface/Headers" ]; then
+	echo "Linking JUMPUserInterface Headers..."
+	ln -s ../JUMPUserInterface/Headers/* .
+fi
 
-echo "Linking JUMPLogger Headers (Includes Log4CocoaTouch and NSLogger..."
-ln -s ../JUMPLogger/Headers/* .
-ln -s ../JUMPLogger/Libraries/Log4CocoaTouch/Headers/* .
-ln -s ../JUMPLogger/Libraries/NSLogger/Headers/* .
+######## ######## ######## ######## ######## ######## ########  
+# if JUMPCore Directory exist...
+if [ -d "../JUMPLogger/Headers" ]; then
+	echo "Linking JUMPLogger Headers (Includes Log4CocoaTouch and NSLogger..."
+	ln -s ../JUMPLogger/Headers/* .
+	ln -s ../JUMPLogger/Libraries/Log4CocoaTouch/Headers/* .
+	ln -s ../JUMPLogger/Libraries/NSLogger/Headers/* .
+fi
