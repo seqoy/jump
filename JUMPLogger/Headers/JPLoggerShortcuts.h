25af0dd8ecfce3753afecefa8fca983440aad17e
Paulo Oliveira <paulo@seqoy.com>
2011-03-26 10:17:58 -0300

    Undefine shortcuts if needed.

--- JUMPLogger/Headers/JPLoggerShortcuts.h
+++ JUMPLogger/Headers/JPLoggerShortcuts.h
@@ -42,6 +42,25 @@
  */
 ///@{ 
 
+//// //// //// //// //// //// //// ////
+// Udenfine if needed.
+#ifdef Debug()
+	#undef Debug()
+#endif
+#ifdef Info()
+	#undef Info()
+#endif
+#ifdef Warn()
+	#undef Warn()
+#endif
+#ifdef Error()
+	#undef Error()
+#endif
+#ifdef Fatal()
+	#undef Fatal()
+#endif
+
+// Define.
 #define Debug(message, ...) DebugException( nil, message, ##__VA_ARGS__)
 #define Info(message, ...) InfoException( nil, message, ##__VA_ARGS__)
 #define Warn(message, ...) WarnException( nil, message, ##__VA_ARGS__)
@@ -57,6 +76,25 @@
  */
 ///@{ 
 
+//// //// //// //// //// //// //// ////
+// Udenfine if needed.
+#ifdef DebugException()
+	#undef DebugException()
+#endif
+#ifdef InfoException()
+	#undef InfoException()
+#endif
+#ifdef WarnException()
+	#undef WarnException()
+#endif
+#ifdef ErrorException()
+	#undef ErrorException()
+#endif
+#ifdef FatalException()
+	#undef FatalException()
+#endif
+
+// Define.
 #define DebugException(exception, message, ...) JPLogIfYouCan( @selector(debugWithMetadata:), __FILE__,  [self class], self, NSStringFromSelector( _cmd ),  __LINE__, [NSString stringWithFormat:message, ##__VA_ARGS__], exception)
 #define InfoException(exception, message, ...) JPLogIfYouCan( @selector(infoWithMetadata:), __FILE__,  [self class], self, NSStringFromSelector( _cmd ),  __LINE__, [NSString stringWithFormat:message, ##__VA_ARGS__], exception)
 #define WarnException(exception, message, ...) JPLogIfYouCan( @selector(warnWithMetadata:), __FILE__,  [self class], self, NSStringFromSelector( _cmd ),  __LINE__, [NSString stringWithFormat:message, ##__VA_ARGS__], exception)
