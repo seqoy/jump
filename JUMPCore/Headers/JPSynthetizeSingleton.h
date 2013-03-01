47b5fa7c2893a60052c073e37334a54e01b455be
Paulo Oliveira <eu@pauloliveira.net>
2013-03-01 17:57:03 -0300

    [JUMPCore] Update Singleton Synthetizer
    Created by Matt Gallagher on 2011/08/23.
    Copyright (c) 2011 Matt Gallagher. All rights reserved.

--- JUMPCore/Headers/JPSynthetizeSingleton.h
+++ JUMPCore/Headers/JPSynthetizeSingleton.h
@@ -4,8 +4,8 @@
 	SynthesizeSingleton
 	CocoaWithLove - http://cocoawithlove.com/
 
-	Created by Matt Gallagher on 20/10/08.
-	Copyright 2009 Matt Gallagher. All rights reserved.
+	Created by Matt Gallagher on 2011/08/23.
+	Copyright (c) 2011 Matt Gallagher. All rights reserved.
 
 	Permission is given to use this source code file without charge in any
 	project, commercial or otherwise, entirely at your risk, with the condition
@@ -21,47 +21,29 @@
  * \copyright Copyright 2009 Matt Gallagher. All rights reserved. 
  */
 
-/** 
- * \def JPSynthetizeSingleton( classname )
- * Automatically synthetize all needed methods to implement the Singleton Pattern on some class.
- * @param classname The class name to sythetize.
- */
-#define JPSynthetizeSingleton( classname ) \
-\
-static classname *sharedInstance = nil; \
-\
-+ (classname *)sharedInstance \
-{ \
-@synchronized(self) \
-{ \
-if (sharedInstance == nil) \
-{ \
-sharedInstance = [[self alloc] init]; \
-} \
-} \
-\
-return sharedInstance; \
-} \
-\
-+ (id)allocWithZone:(NSZone *)zone \
-{ \
-@synchronized(self) \
-{ \
-if (sharedInstance == nil) \
-{ \
-sharedInstance = [super allocWithZone:zone]; \
-return sharedInstance; \
-} \
-} \
-\
-return nil; \
-} \
-\
-- (id)copyWithZone:(NSZone *)zone \
-{ \
-return self; \
-} \
-\
+//
+//  CWLSynthesizeSingleton.h
+//  CocoaWithLove
+//
+//  Created by Matt Gallagher on 2011/08/23.
+//  Copyright (c) 2011 Matt Gallagher. All rights reserved.
+//
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#import <objc/runtime.h>
+
+#define CWL_DECLARE_SINGLETON_FOR_CLASS_WITH_ACCESSOR(classname, accessorMethodName) \
++ (classname *)accessorMethodName;
+
+#if __has_feature(objc_arc)
+#define CWL_SYNTHESIZE_SINGLETON_RETAIN_METHODS
+#else
+#define CWL_SYNTHESIZE_SINGLETON_RETAIN_METHODS \
 - (id)retain \
 { \
 return self; \
@@ -72,7 +54,7 @@ return self; \
 return NSUIntegerMax; \
 } \
 \
-- (void)release \
+- (oneway void)release \
 { \
 } \
 \
@@ -80,3 +62,60 @@ return NSUIntegerMax; \
 { \
 return self; \
 }
+#endif
+
+#define CWL_SYNTHESIZE_SINGLETON_FOR_CLASS_WITH_ACCESSOR(classname, accessorMethodName) \
+\
+static classname *accessorMethodName##Instance = nil; \
+\
++ (classname *)accessorMethodName \
+{ \
+@synchronized(self) \
+{ \
+if (accessorMethodName##Instance == nil) \
+{ \
+accessorMethodName##Instance = [super allocWithZone:NULL]; \
+accessorMethodName##Instance = [accessorMethodName##Instance init]; \
+method_exchangeImplementations(\
+class_getClassMethod([accessorMethodName##Instance class], @selector(accessorMethodName)),\
+class_getClassMethod([accessorMethodName##Instance class], @selector(cwl_lockless_##accessorMethodName)));\
+method_exchangeImplementations(\
+class_getInstanceMethod([accessorMethodName##Instance class], @selector(init)),\
+class_getInstanceMethod([accessorMethodName##Instance class], @selector(cwl_onlyInitOnce)));\
+} \
+} \
+\
+return accessorMethodName##Instance; \
+} \
+\
++ (classname *)cwl_lockless_##accessorMethodName \
+{ \
+return accessorMethodName##Instance; \
+} \
+\
++ (id)allocWithZone:(NSZone *)zone \
+{ \
+return [self accessorMethodName]; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return self; \
+} \
+- (id)cwl_onlyInitOnce \
+{ \
+return self;\
+} \
+\
+CWL_SYNTHESIZE_SINGLETON_RETAIN_METHODS
+
+#define CWL_DECLARE_SINGLETON_FOR_CLASS(classname) CWL_DECLARE_SINGLETON_FOR_CLASS_WITH_ACCESSOR(classname, shared##classname)
+#define CWL_SYNTHESIZE_SINGLETON_FOR_CLASS(classname) CWL_SYNTHESIZE_SINGLETON_FOR_CLASS_WITH_ACCESSOR(classname, shared##classname)
+
+
+/**
+ * \def JPSynthetizeSingleton( classname )
+ * Automatically synthetize all needed methods to implement the Singleton Pattern on some class.
+ * @param classname The class name to sythetize.
+ */
+#define JPSynthetizeSingleton( classname ) CWL_SYNTHESIZE_SINGLETON_FOR_CLASS_WITH_ACCESSOR( classname, sharedInstance )
